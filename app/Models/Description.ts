db03f6295720b826ce6a8415f9d00e74630f42b1
David <xkromkad@stuba.sk>
2023-04-21 19:54:38 +0200

    Add: .description exercise database model, controller, migration

---
+++ app/Models/Description.ts
@@ -0,0 +1,53 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import DescriptionType from './DescriptionType'
+
+export default class Description extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()
+  public image: string
+
+  @column()
+  public search: string
+
+  @column()
+  public see: string
+
+  @column()
+  public emotion: string
+
+  @column()
+  public fact: string
+
+  @column()
+  public imagine: string
+
+  @column()
+  public future: string
+
+  @column()
+  public story: string
+
+  @column()
+  public behaviour: string
+
+  @column()
+  public typeId: number
+
+  @belongsTo(() => DescriptionType, {
+    localKey: 'id',
+    foreignKey: 'typeId',
+  })
+  public admin: BelongsTo<typeof DescriptionType>
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
---
+++ database/migrations/1681973291070_descriptions.ts
@@ -0,0 +1,38 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'descriptions'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title', 30)
+      table.string('image', 60)
+      table.string('search', 60)
+      table.string('see', 60)
+      table.string('emotion', 60)
+      table.string('fact', 60)
+      table.string('imagine', 60)
+      table.string('future', 60)
+      table.string('story', 60)
+      table.string('behaviour', 60)
+      table
+        .integer('type_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('description_types')
+        .onDelete('CASCADE')
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
